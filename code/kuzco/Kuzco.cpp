8ba9ac9ea1cfe25308c40ed5cf26e1f46daa6f1d
Borislav Stanimirov <b.stanimirov@abv.bg>
2020-05-28 18:29:15 +0300

    License in source files

--- code/kuzco/Kuzco.cpp
+++ code/kuzco/Kuzco.cpp
@@ -1,3 +1,10 @@
+// kuzco
+// Copyright (c) 2020 Borislav Stanimirov
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE.txt or copy at
+// https://opensource.org/licenses/MIT
+//
 #include "Kuzco.hpp"
 
 #include <cassert>
