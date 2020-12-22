e7c861de390c8220aac58db34ddb470ee5853ca9
Borislav Stanimirov <b.stanimirov@abv.bg>
2020-12-22 16:10:40 +0200

    Initial version of CoW vector node

--- code/kuzco/Vector.hpp
+++ code/kuzco/Vector.hpp
@@ -21,29 +21,196 @@ public:
 
     using Wrapped = WrappedVector;
 
-    //Vector& operator=(const Vector&) = delete;
-
     using Super::operator=;
 
     // std::vector-like
     using value_type = typename Wrapped::value_type;
+    using size_type = typename Wrapped::size_type;
     using iterator = typename Wrapped::iterator;
     using const_iterator = typename Wrapped::const_iterator;
+    using reverse_iterator = typename Wrapped::reverse_iterator;
+    using const_reverse_iterator = typename Wrapped::const_reverse_iterator;
+    using pointer = typename Wrapped::pointer;
+    using const_pointer = typename Wrapped::const_pointer;
+    using reference = typename Wrapped::reference;
+    using const_reference = typename Wrapped::const_reference;
 
     const size_t size() const { return this->get()->size(); }
+    const size_t capacity() const { return this->get()->capacity(); }
     const bool empty() const { return this->get()->empty(); }
 
-    const value_type& operator[](size_t i) const { return this->get()->operator*[](i); }
-    value_type& operator[](size_t i) { return this->get()->operator*[](i); }
+    pointer data() { return this->get()->data(); }
+    const_pointer data() const { return this->get()->data(); }
 
-    iteartor begin() { return this->get()->begin(); }
+    const value_type& operator[](size_type i) const { return this->get()->operator[](i); }
+    value_type& operator[](size_type i) { return this->get()->operator[](i); }
+
+    iterator begin() { return this->get()->begin(); }
     iterator end() { return this->get()->end(); }
-    const_iteartor begin() const { return this->get()->begin(); }
+    const_iterator begin() const { return this->get()->begin(); }
     const_iterator end() const { return this->get()->end(); }
+    const_iterator cbegin() const { return begin(); }
+    const_iterator cend() const { return end(); }
+
+    reverse_iterator rbegin() { return this->get()->rbegin(); }
+    reverse_iterator rend() { return this->get()->rend(); }
+    const_reverse_iterator rbegin() const { return this->get()->rbegin(); }
+    const_reverse_iterator rend() const { return this->get()->rend(); }
+    const_reverse_iterator crbegin() const { return rbegin(); }
+    const_reverse_iterator crend() const { return rend(); }
+
+    reference front() { return this->get()->front(); }
+    const_reference front() const { return this->get()->front(); }
+    reference back() { return this->get()->back(); }
+    const_reference back() const { return this->get()->back(); }
+
+    template <typename... Fwd>
+    void assign(Fwd&&... fwd)
+    {
+        if (!unique())
+        {
+            auto newVec = impl::Data<Wrapped>::construct(std::forward<Fwd>(fwd)...);
+            this->replaceWith(std::move(newVec));
+        }
+        else
+        {
+            this->qget()->assign(std::forward<Fwd>(fwd)...);
+        }
+    }
+
+    void assign(std::initializer_list<value_type> ilist)
+    {
+        if (!unique())
+        {
+            auto newVec = impl::Data<Wrapped>::construct(ilist);
+            this->replaceWith(std::move(newVec));
+        }
+        else
+        {
+            this->qget()->assign(ilist);
+        }
+    }
+
+    iterator insert(const_iterator pos, const value_type& val)
+    {
+        if (!unique())
+        {
+            inserter i(*this, pos, 1);
+            return i.v().insert(i.v().end(), val);
+        }
+        else
+        {
+            return this->qget()->insert(pos, val);
+        }
+    }
+
+    iterator insert(const_iterator pos, std::initializer_list<value_type> ilist)
+    {
+        if (!unique())
+        {
+            inserter i(*this, pos, ilist.size());
+            return i.v().insert(i.v().end(), ilist);
+        }
+        else
+        {
+            return this->qget()->insert(pos, ilist);
+        }
+    }
+
+    iterator erase(const_iterator pos)
+    {
+        if (!unique())
+        {
+            return shrink(pos, 1);
+        }
+        else
+        {
+            return this->qget()->erase(pos);
+        }
+    }
+
+    iterator erase(const_iterator b, const_iterator e)
+    {
+        if (!unique())
+        {
+            return shrink(b, e-b);
+        }
+        else
+        {
+            return this->qget()->erase(b, e);
+        }
+    }
+
+    void reserve(size_type cap)
+    {
+        if (!unique())
+        {
+            auto oldVec = this->qget();
+            if (oldVec->capacity() >= cap) return; // nothing to do
+            auto newVec = impl::Data<Wrapped>::construct();
+            newVec.qdata->reserve(cap);
+            *newVec.qdata = *oldVec;
+            this->replaceWith(std::move(newVec));
+        }
+        else
+        {
+            this->qget()->reserve(cap);
+        }
+    }
+
+    void resize(size_type count)
+    {
+        auto oldVec = this->qget();
+        if (!unique())
+        {
+            if (oldVec->size() == count) return; // nothing to do
+            auto newVec = impl::Data<Wrapped>::construct();
+            if (count < oldVec->size())
+            {
+                newVec.qdata->assign(oldVec->begin(), oldVec->begin() + count);
+            }
+            else
+            {
+                newVec.qdata->reserve(count);
+                *newVec.qdata = *oldVec;
+                newVec.qdata->resize(count);
+            }
+            this->replaceWith(std::move(newVec));
+        }
+        else
+        {
+            oldVec->resize(count);
+        }
+    }
+
+    void resize(size_type count, const value_type& val)
+    {
+        auto oldVec = this->qget();
+        if (!unique())
+        {
+            if (oldVec->size() == count) return; // nothing to do
+            auto newVec = impl::Data<Wrapped>::construct();
+            if (count < oldVec->size())
+            {
+                newVec.qdata->assign(oldVec->begin(), oldVec->begin() + count);
+            }
+            else
+            {
+                newVec.qdata->reserve(count);
+                *newVec.qdata = *oldVec;
+                newVec.qdata->resize(count, val);
+            }
+            this->replaceWith(std::move(newVec));
+        }
+        else
+        {
+            oldVec->resize(count, val);
+        }
+    }
 
     void clear()
     {
-        if (!unique)
+        if (!unique())
         {
             auto newVec = impl::Data<Wrapped>::construct();
             this->replaceWith(std::move(newVec));
@@ -57,18 +224,86 @@ public:
     template <typename... Args>
     value_type& emplace_back(Args&&... args)
     {
-        if (!unique())
-        {
-            auto oldVec = this->qget();
-            auto newVec = impl::Data<Wrapped>::construct();
-            newVec.qdata->reserve(oldVec.size() + 1);
-            *newVec.qdata = *oldVec;
-            this->replaceWith(std::move(newVec));
-        }
+        prepare_add_one();
         return this->qget()->emplace_back(std::forward<Args>(args)...);
     }
 
+    void push_back(const value_type& val)
+    {
+        prepare_add_one();
+        this->qget()->push_back(val);
+    }
+
+    void pop_back()
+    {
+        auto oldVec = this->qget();
+        if (!unique())
+        {
+            auto newVec = impl::Data<Wrapped>::construct();
+            newVec.qdata->assign(oldVec->begin(), oldVec->end() - 1);
+            this->replaceWith(std::move(newVec));
+        }
+        else
+        {
+            oldVec->pop_back();
+        }
+    }
+
 private:
+    // used by push/emplace_back()
+    void prepare_add_one()
+    {
+        if (unique()) return;
+
+        inserter i(*this, this->qget()->cend(), 1);
+    }
+
+    // used by insert/emplace
+    struct inserter
+    {
+        inserter(Vector& b, typename Wrapped::const_iterator pos, size_type count)
+            : m_v(b)
+            , m_oldVec(b.qget())
+            , m_pos(pos)
+            , m_count(count)
+            , m_newVec(impl::Data<Wrapped>::construct())
+        {
+            v().reserve(m_oldVec->size() + count);
+            v().assign(m_oldVec->cbegin(), pos);
+        }
+
+        ~inserter()
+        {
+            v().insert(v().cend(), m_pos, m_oldVec->cend());
+            m_v.replaceWith(std::move(m_newVec));
+        }
+
+        Wrapped& v()
+        {
+            return *m_newVec.qdata;
+        }
+
+        Vector& m_v;
+        Wrapped* m_oldVec;
+        typename Wrapped::const_iterator m_pos;
+        size_type m_count;
+        impl::Data<Wrapped> m_newVec;
+    };
+
+    iterator shrink(const_iterator pos, size_type by)
+    {
+        auto oldVec = this->qget();
+        if (pos + by > oldVec->cend()) throw 0;
+        auto newVec = impl::Data<Wrapped>::construct();
+        auto& v = *newVec.qdata;
+        v.reserve(oldVec->size() - by);
+        v.assign(oldVec->cbegin(), pos);
+        v.insert(v.end(), pos + by, oldVec->cend());
+        auto ret = v.begin() + (pos - oldVec->cbegin());
+        replaceWith(std::move(newVec));
+        return ret;
+    }
+
     using Super::operator*;
     using Super::operator->;
 };
--- test/t-Vector.cpp
+++ test/t-Vector.cpp
@@ -6,4 +6,211 @@
 // https://opensource.org/licenses/MIT
 //
 #include <kuzco/Vector.hpp>
+#include "doctest.hpp"
 
+#include <string>
+
+TEST_SUITE_BEGIN("Kuzco vector");
+
+template <template<typename> class State>
+void testState()
+{
+    {
+        State<int> state;
+        auto di = state.detach();
+        CHECK(di->vec.size() == 0);
+        CHECK(di->vec.capacity() == 0);
+        CHECK(di->vec.begin() == di->vec.end());
+        CHECK(di->vec.cbegin() == di->vec.cend());
+        CHECK(di->vec.empty());
+
+        auto d = state.detach()->vec.data();
+        state.transaction()->vec.reserve(2);
+        di = state.detach();
+        CHECK(di->vec.capacity() >= 2);
+        CHECK(d != di->vec.data());
+
+        state.transaction()->vec.resize(3, 8);
+        di = state.detach();
+        CHECK(di->vec.capacity() >= 3);
+        CHECK(di->vec.size() == 3);
+        CHECK(di->vec.front() == 8);
+        CHECK(di->vec.back() == 8);
+
+        state.transaction()->vec.clear();
+        di = state.detach();
+        CHECK(di->vec.size() == 0);
+        CHECK(di->vec.begin() == di->vec.end());
+        CHECK(di->vec.cbegin() == di->vec.cend());
+        CHECK(di->vec.empty());
+
+        state.transaction()->vec.push_back(5);
+        di = state.detach();
+        CHECK(di->vec.size() == 1);
+        CHECK(di->vec[0] == 5);
+        auto it = di->vec.begin();
+        CHECK(it == di->vec.cbegin());
+        CHECK(*it == 5);
+        ++it;
+        CHECK(it == di->vec.end());
+        CHECK(it == di->vec.cend());
+
+        auto i = state.transaction()->vec.emplace_back(3);
+        CHECK(i == 3);
+        CHECK(state.detach()->vec.size() == 2);
+
+        {
+            auto t = state.transaction();
+            auto rit = t->vec.rbegin();
+            CHECK(*rit == 3);
+            ++rit;
+            *rit = 12;
+            ++rit;
+            CHECK(rit == t->vec.rend());
+            CHECK(rit == t->vec.crend());
+            CHECK(t->vec.front() == 12);
+            CHECK(t->vec.back() == 3);
+        }
+
+        {
+            auto t = state.transaction();
+            auto i = t->vec.insert(t->vec.cbegin(), 53);
+            CHECK(i == t->vec.cbegin());
+        }
+        CHECK(state.detach()->vec.capacity() >= 3);
+
+        {
+            auto t = state.transaction();
+            auto i = t->vec.insert(t->vec.cbegin() + 2, 90);
+            CHECK(i == t->vec.cbegin() + 2);
+        }
+
+        {
+            auto t = state.transaction();
+            t->vec.insert(t->vec.cbegin() + 4, 17);
+        }
+        {
+            auto t = state.transaction();
+            auto i = t->vec.insert(t->vec.cend(), 6);
+            CHECK(i == t->vec.cend() - 1);
+        }
+        {
+            auto t = state.transaction();
+            t->vec.insert(t->vec.cbegin(), { 1, 2 });
+        }
+
+        di = state.detach();
+        int ints[] = { 1, 2, 53, 12, 90, 3, 17, 6 };
+        CHECK(di->vec.capacity() >= 8);
+        CHECK(di->vec.size() == 8);
+        CHECK(memcmp(di->vec.data(), ints, sizeof(ints)) == 0);
+
+        state.transaction()->vec.pop_back();
+        di = state.detach();
+        CHECK(di->vec.size() == 7);
+        CHECK(memcmp(di->vec.data(), ints, sizeof(ints) - sizeof(int)) == 0);
+
+        state.transaction()->vec.resize(8);
+        di = state.detach();
+        CHECK(di->vec.size() == 8);
+        ints[7] = 0;
+        CHECK(memcmp(di->vec.data(), ints, sizeof(ints)) == 0);
+
+        {
+            auto t = state.transaction();
+            auto r = t->vec.erase(t->vec.cbegin());
+            CHECK(r == t->vec.cbegin());
+        }
+        di = state.detach();
+        CHECK(di->vec.size() == 7);
+        CHECK(di->vec.front() == 2);
+        CHECK(memcmp(di->vec.data(), ints + 1, di->vec.size() * sizeof(int)) == 0);
+
+        {
+            auto t = state.transaction();
+            auto r = t->vec.erase(t->vec.cbegin() + 2, t->vec.cbegin() + 4);
+            CHECK(r == t->vec.cbegin() + 2);
+        }
+        di = state.detach();
+        CHECK(di->vec.size() == 5);
+        CHECK(di->vec[3] == 17);
+    }
+
+    {
+        State<std::string> state;
+        state.transaction()->vec.assign({"as", "df"});
+        CHECK(state.detach()->vec.size() == 2);
+        std::string s1 = "the quick brown fox jumped over the lazy dog 1234567890";
+        state.transaction()->vec.emplace_back(s1);
+        CHECK(state.detach()->vec.back() == s1);
+    }
+}
+
+template <typename T>
+using TVec = kuzco::Vector<std::vector<T>>;
+
+template <typename T>
+struct VecState
+{
+    TVec<T> vec;
+};
+
+template <typename T>
+struct UniqueState
+{
+    VecState<T> state;
+    auto detach() const { return &state; }
+    auto transaction() { return &state; }
+};
+
+TEST_CASE("Vector is a vector")
+{
+    testState<UniqueState>();
+}
+
+template <typename T>
+class State : private kuzco::Root<VecState<T>> {
+public:
+    State()
+        : kuzco::Root<VecState<T>>(kuzco::Node<VecState<T>>())
+    {}
+
+    struct Transaction {
+    public:
+        Transaction(State& s)
+            : m_ptr(s.beginTransaction())
+            , m_state(s)
+        {}
+
+        Transaction(const Transaction&) = delete;
+        Transaction& operator=(const Transaction&) = delete;
+        Transaction(Transaction&&) = delete;
+        Transaction& operator=(Transaction&&) = delete;
+
+        void cancel() { m_cancelled = true; }
+
+        VecState<T>* operator->() { return m_ptr; }
+        VecState<T>& operator*() { return *m_ptr; }
+
+        ~Transaction() {
+            bool store = !m_cancelled && !std::uncaught_exceptions();
+            m_state.endTransaction(store);
+        }
+    private:
+        VecState<T>* m_ptr;
+        State& m_state;
+        bool m_cancelled = false;
+    };
+
+    Transaction transaction() {
+        return Transaction(*this);
+    }
+
+    using Root<VecState<T>>::detach;
+    using Root<VecState<T>>::detachedPayload;
+};
+
+TEST_CASE("State vector")
+{
+    testState<State>();
+}
