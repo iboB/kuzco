235aea17a583370e5184c61b1eabc273cd2462e6
Borislav Stanimirov <b.stanimirov@abv.bg>
2020-12-28 21:43:16 +0200

    NodeRef

---
+++ code/kuzco/NodeRef.hpp
@@ -0,0 +1,43 @@
+// kuzco
+// Copyright (c) 2020 Borislav Stanimirov
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE.txt or copy at
+// https://opensource.org/licenses/MIT
+//
+#pragma once
+
+#include "Node.hpp"
+
+namespace kuzco
+{
+
+template <typename T>
+class NodeRef
+{
+public:
+    using Node = kuzco::Node<T>;
+
+    NodeRef() = default;
+    explicit NodeRef(Node& n) : m_node(&n) {}
+
+    explicit operator bool() const { return !!m_node; }
+
+    void reset() { m_node = nullptr; }
+    void reset(Node& n) { m_node = &n; }
+
+    Node& operator=(Node&& other) { return m_node->operator=(std::move(other)); }
+
+    template <typename U, std::enable_if_t<std::is_assignable_v<T&, U>, int> = 0>
+    Node& operator=(U&& u) { return m_node->operator=(std::forward<U>(u)); }
+
+    const Node& r() const { return m_node->r(); }
+
+    T* operator->() { return m_node->operator->(); }
+    T& operator*() { return m_node->operator*(); }
+
+private:
+    Node* m_node = nullptr;
+};
+
+}
---
+++ test/t-NodeRef.cpp
@@ -0,0 +1,40 @@
+// kuzco
+// Copyright (c) 2020 Borislav Stanimirov
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE.txt or copy at
+// https://opensource.org/licenses/MIT
+//
+#include <kuzco/NodeRef.hpp>
+
+#include <doctest/doctest.h>
+
+TEST_SUITE_BEGIN("Kuzco NodeRef");
+
+using namespace kuzco;
+
+TEST_CASE("Basic")
+{
+    kuzco::NodeRef<int> ref;
+    CHECK(!ref);
+
+    kuzco::Node<int> i = 5;
+    auto d = i.detach();
+
+    ref.reset(i);
+    CHECK(!!ref);
+
+    CHECK(*ref == 5);
+    *ref = 8;
+    CHECK(*i == 8);
+
+    kuzco::Node<int> i2 = 33;
+    auto d2 = i2.detach();
+    ref = std::move(i2);
+
+    CHECK(d2 == i);
+    CHECK(i.detach() != d);
+
+    ref.reset();
+    CHECK(!ref);
+}
