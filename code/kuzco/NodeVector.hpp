2bb9645fc0e171463172b70d0695c3810c2d0793
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-03-04 14:13:32 +0200

    clang compilation fix

--- code/kuzco/NodeVector.hpp
+++ code/kuzco/NodeVector.hpp
@@ -18,7 +18,15 @@ class NodeVector : public VectorImpl<WrappedVector<Node<T>>>
 {
 public:
     using Super = VectorImpl<WrappedVector<Node<T>>>;
+#if defined(_MSC_VER)
+    // For some weird reason clang requires *typename* here and msvc requires *no-typename* here
+    // gcc accepts both
+    // It seems to me that msvc is right as this is a function (ctor) and not a type
+    // anyway... we must #ifdef to work around this discrepancy
     using Super::VectorImpl;
+#else
+    using typename Super::VectorImpl;
+#endif
 
     using typename Super::value_type;
     using typename Super::size_type;
