61170c5e8ace3f9e6c207eb7f8d0d844b7fc89e2
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-12-29 11:36:19 +0200

    Removed obsolete handle. SharedState test

--- test/t-SharedState.cpp
+++ test/t-SharedState.cpp
@@ -1,7 +1,6 @@
 // Copyright (c) Borislav Stanimirov
 // SPDX-License-Identifier: MIT
 //
-#include <kuzco/Handle.hpp>
 #include <kuzco/State.hpp>
 
 #include <doctest/doctest.h>
@@ -14,13 +13,13 @@ struct HandleTestType : public doctest::util::lifetime_counter<HandleTestType>
     int n = 1;
 };
 
-TEST_CASE("Simple handle test")
+TEST_CASE("Simple SharedState test")
 {
     HandleTestType::lifetime_stats stats;
     doctest::util::lifetime_counter_sentry sentry(stats);
 
     kuzco::State<HandleTestType> r1({});
-    kuzco::Handle<HandleTestType> h1(r1.detach());
+    auto ss1 = r1.sharedState();
 
     {
         auto mut = r1.beginTransaction();
@@ -28,10 +27,12 @@ TEST_CASE("Simple handle test")
         r1.endTransaction();
     }
 
-    auto r = h1.load();
-    CHECK(r->n == 1);
+    auto r = ss1.detach();
+    CHECK(r->n == 123);
 
-    kuzco::Handle<HandleTestType> h2(r1.detach());
+    auto ss2 = r1.sharedState();
+    r = ss2.detach();
+    CHECK(r->n == 123);
 
     {
         auto mut = r1.beginTransaction();
@@ -39,28 +40,9 @@ TEST_CASE("Simple handle test")
         r1.endTransaction();
     }
 
-    r = h1.load();
-    CHECK(r->n == 1);
-
-    r = h2.load();
-    CHECK(r->n == 123);
-
-    h1.store(r1.detach());
-    r = h1.load();
+    r = ss1.detach();
     CHECK(r->n == 456);
-}
 
-TEST_CASE("Simple opt handle test")
-{
-    HandleTestType::lifetime_stats stats;
-    doctest::util::lifetime_counter_sentry sentry(stats);
-
-    kuzco::OptHandle<HandleTestType> h1({});
-    auto r = h1.load();
-    CHECK(!r);
-
-    kuzco::State<HandleTestType> r1({});
-    h1.store(r1.detach());
-    r = h1.load();
-    CHECK(r->n == 1);
+    r = ss2.detach();
+    CHECK(r->n == 456);
 }
